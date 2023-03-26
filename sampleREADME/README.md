dbb71e82bb1880236a3bc7cc691354d2acd53c93
Anthony-A-Perez <ap.erez@me.com>
2023-03-26 14:23:00 -0700

    created generate markdown file

---
+++ sampleREADME/README.md
@@ -0,0 +1,44 @@
+# **README Generator**
+
+## Description
+
+This is a command line application that takes in user project information and generates a professional README
+
+## Table on Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+No installation required
+
+## Usage
+
+Type 'node index' into the command line and follow prompts
+
+## License
+
+    *This project is covered under the gpl license:*
+[![gpl License](https://img.shields.io/badge/License-gpl-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+## Contributing
+
+Contribution inquiries can be made through the contact information provided
+
+## Tests
+
+test are currently in development˝
+
+## Questions
+
+If you have questions about this project, please contact me at email@me.com, or [Github](https://github.com/Anthony-A-Perez).
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,84 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} License](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'mit') {
+    return `https://www.mit.edu/~amini/LICENSE.md`;
+  }
+  if (license === 'gpl') {
+    return `https://www.gnu.org/licenses/gpl-3.0.en.html`;
+  }
+  if (license === 'wtfpl') {
+    return `http://www.wtfpl.net/about/`
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## License\n
+    *This project is covered under the ${license} license:*`
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# **${data.title}**
+
+## Description
+
+${data.description}
+
+## Table on Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+
+## Contributing
+
+${data.contributors}
+
+## Tests
+
+${data.test}˝
+
+## Questions
+
+If you have questions about this project, please contact me at ${data.email}, or [Github](https://github.com/${data.github}).`;
+
+
+}
+
+module.exports = generateMarkdown;
